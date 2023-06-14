b1423225cd260d91c0084921ab61dc1a66403db7
smargetsam <95829951+smargetsam@users.noreply.github.com>
2023-06-15 01:40:29 +1000

    created mouseenter based drawing etchasketch. TODO: add button to clear entry and reset squares to default colour

---
+++ logic.js
@@ -0,0 +1,25 @@
+
+for (let i = 0; i < 128; i++) {
+
+var div = document.createElement("div");
+div.style.width = "30px";
+div.style.height = "30px";
+div.style.background = "#D3D3D3";
+
+
+
+div.addEventListener("mouseover", function onmouseenter(event) {
+    // 👇️ change background color
+    event.target.style.backgroundColor = 'black';
+
+    
+  
+    // 👇️ optionally change text color
+    // event.target.style.color = 'white';
+  });
+  
+document.getElementById("main").appendChild(div);
+}
+
+
+
